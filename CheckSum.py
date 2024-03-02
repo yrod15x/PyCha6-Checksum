a5a8eba4731cb787c2b3d4ae5adfdc2ef3ec6ccd
yrod15x <megasoldier15x@hotmail.com>
2024-03-02 08:46:18 -0500

    Programa completo y testado.

--- CheckSum.py
+++ CheckSum.py
@@ -1,8 +1,9 @@
 class Checksum:
     def __init__(self) -> None:
         self._numero = ''
+        self.__resultado = 0
     
-    def entrada_dato(self):
+    def entrada_dato(self)->None:
         while True:
             self._numero = input('Ingrese un numero entrero >> ')
             try:
@@ -11,3 +12,14 @@ class Checksum:
             except Exception as e:
                 print('El dato ingresado no es nunumero')
                 print(e)
+        print()
+
+    def __ejecutar_checksum(self)->None:
+        suma = []
+        for index, val in enumerate(self._numero):
+           suma.append(int(val) * (index + 1))
+        self.__resultado = sum(suma) % 10
+        
+    def mostrar_checksum(self)->int:
+        self.__ejecutar_checksum()
+        return self.__resultado
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from CheckSum import Checksum
 
+print("""*** Programa que muestra el restante de sumar los digitos de un numero 
+entero que a su vez son multiplicados por una sucesion de 1 hasta la cantidad
+de estos digitos *** """)
+print()
+
 suma = Checksum()
 suma.entrada_dato()
+
+print(f"El resultado del CHECKSUM es {suma.mostrar_checksum()}")
+print()
